a7d9f9ec1dc549bb5c14058403471e9ee804970c
Deividi-Lucas <deividibaby6@gmail.com>
2023-05-11 20:17:05 -0400

    Docs: command in comment

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 
+// Command in SQL
+
+// Create database name_of_database;
+
+// create table name_of_table(info_of_table); //example column varchar(25)
+
+// use name_of_database;
+
+// insert into Name_of_database(name_of_table) values('Info');
+
 app.get('/', (req, res) => {
-  const result = db.pool.query('select * from list')
-  res.send(result)
+  const result = db.pool.query('show databases;') // database no selected
+  res.send()
 })
 
 app.post('/', (req, res) => {
   let text = req.body
-  const result = db.pool.query()
-  res.send(result)
+  res.send(text)
 })
 
 app.listen('3000', () => {
